c0a7e2de072bce905834190ae183f7cf96c5c7a3
ScorpionIT <blackyellowguy1@gmail.com>
2022-12-16 19:33:52 +0400

    hpc labs 2022

---
+++ Matmul/kernel.cu
@@ -0,0 +1,106 @@
+﻿#include <algorithm>
+#include <cassert>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <vector>
+#include <chrono>
+
+using namespace std;
+
+__global__ void matrixMul(const int* a, const int* b, int* c, int N) {
+    // Compute each thread's global row and column index
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // Iterate over row, and down column
+    c[row * N + col] = 0;
+    for (int k = 0; k < N; k++) {
+        // Accumulate results for a single element
+        c[row * N + col] += a[row * N + k] * b[k * N + col];
+    }
+}
+
+// Check result on the CPU
+void verify_result(vector<int>& a, vector<int>& b, vector<int>& c, int N) {
+    chrono::system_clock::time_point startCpu = chrono::system_clock::now();
+    // For every row...
+    for (int i = 0; i < N; i++) {
+        // For every column...
+        for (int j = 0; j < N; j++) {
+            // For every element in the row-column pair
+            int tmp = 0;
+
+            for (int k = 0; k < N; k++) {
+                // Accumulate the partial results
+                tmp += a[i * N + k] * b[k * N + j];
+            }
+
+            // Check against the CPU result
+            assert(tmp == c[i * N + j]);
+        }
+    }
+    chrono::system_clock::time_point endCpu = chrono::system_clock::now();
+    auto timeCpu = chrono::duration_cast<chrono::nanoseconds>(endCpu - startCpu).count();
+    cout << "CPU TIME : " << timeCpu << "ns\n";
+}
+
+int main() {
+    // Matrix size of 1024 x 1024;
+    int N = 1 << 10; //2 in power of 10
+
+    // Size (in bytes) of matrix
+    size_t bytes = N * N * sizeof(int);
+
+    // Host vectors
+    vector<int> h_a(N * N);
+    vector<int> h_b(N * N);
+    vector<int> h_c(N * N);
+
+    // Initialize matrices
+    generate(h_a.begin(), h_a.end(), []() { return rand() % 100; });
+    generate(h_b.begin(), h_b.end(), []() { return rand() % 100; });
+
+    // Allocate device memory
+    int* d_a, * d_b, * d_c;
+    cudaMalloc(&d_a, bytes);
+    cudaMalloc(&d_b, bytes);
+    cudaMalloc(&d_c, bytes);
+
+    // Copy data to the device
+    cudaMemcpy(d_a, h_a.data(), bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b.data(), bytes, cudaMemcpyHostToDevice);
+
+    // Threads per CTA dimension
+    int THREADS = 32;
+
+    // Blocks per grid dimension (assumes THREADS divides N evenly)
+    int BLOCKS = N / THREADS;
+
+    // Use dim3 structs for block  and grid dimensions
+    dim3 threads(THREADS, THREADS);
+    dim3 blocks(BLOCKS, BLOCKS);
+
+    chrono::system_clock::time_point start = chrono::system_clock::now();
+
+    // Launch kernel
+    matrixMul << <blocks, threads >> > (d_a, d_b, d_c, N);
+
+    chrono::system_clock::time_point end = chrono::system_clock::now();
+    auto time = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+
+    // Copy back to the host
+    cudaMemcpy(h_c.data(), d_c, bytes, cudaMemcpyDeviceToHost);
+
+    cout << "GPU TIME : " << time << "ns\n";
+    // Check result
+    verify_result(h_a, h_b, h_c, N);
+
+
+    // Free memory on device
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+
+    return 0;
+}
---
+++ VectorSum/kernel.cu
@@ -0,0 +1,110 @@
+﻿#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+#include <chrono>
+#include <iostream>
+
+#define SIZE 256
+#define SHMEM_SIZE 256 * 4
+
+using namespace std;
+
+void verify_result(int* h_v, int N, int GpuRes) {
+	chrono::system_clock::time_point startCpu = chrono::system_clock::now();
+	for (int i = 1; i < N; i++) {
+		h_v[0] += h_v[i];
+	}
+	chrono::system_clock::time_point endCpu = chrono::system_clock::now();
+	auto timeCpu = chrono::duration_cast<chrono::nanoseconds>(endCpu - startCpu).count();
+	cout << "CPU TIME : " << timeCpu << "ns\n";
+	//assert(h_v[0] == GpuRes);
+	printf("CPU res is %d \n", h_v[0]);
+}
+
+__global__ void sum_reduction(int* v, int* v_r) {
+	// Allocate shared memory
+	__shared__ int partial_sum[SHMEM_SIZE];
+
+	// Calculate thread ID
+	int tid = blockIdx.x * blockDim.x + threadIdx.x;
+
+	// Load elements into shared memory
+	partial_sum[threadIdx.x] = v[tid];
+	__syncthreads();
+
+	// Start at 1/2 block stride and divide by two each iteration
+	for (int s = blockDim.x / 2; s > 0; s >>= 1) {
+		// Each thread does work unless it is further than the stride
+		if (threadIdx.x < s) {
+			partial_sum[threadIdx.x] += partial_sum[threadIdx.x + s];
+		}
+		__syncthreads();
+	}
+
+	// Let the thread 0 for this block write it's result to main memory
+	// Result is inexed by this block
+	if (threadIdx.x == 0) {
+		v_r[blockIdx.x] = partial_sum[0];
+	}
+}
+
+void initialize_vector(int* v, int n) {
+	for (int i = 0; i < n; i++) {
+		v[i] = rand() % 10;
+	}
+}
+
+int main() {
+	// Vector size
+	int n = 1 << 8;
+	size_t bytes = n * sizeof(int);
+
+	// Original vector and result vector
+	int* h_v, * h_v_r;
+	int* d_v, * d_v_r;
+
+	// Allocate memory
+	h_v = (int*)malloc(bytes);
+	h_v_r = (int*)malloc(bytes);
+	cudaMalloc(&d_v, bytes);
+	cudaMalloc(&d_v_r, bytes);
+
+	// Initialize vector
+	initialize_vector(h_v, n);
+
+	// Copy to device
+	cudaMemcpy(d_v, h_v, bytes, cudaMemcpyHostToDevice);
+
+	// TB Size
+	int TB_SIZE = SIZE;
+
+	// Grid Size (No padding)
+	int GRID_SIZE = n / TB_SIZE;
+
+	chrono::system_clock::time_point start = chrono::system_clock::now();
+
+	// Call kernel
+	sum_reduction << <GRID_SIZE, TB_SIZE >> > (d_v, d_v_r);
+
+	sum_reduction << <1, TB_SIZE >> > (d_v_r, d_v_r);
+
+	chrono::system_clock::time_point end = chrono::system_clock::now();
+	auto time = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+
+	// Copy to host;
+	cudaMemcpy(h_v_r, d_v_r, bytes, cudaMemcpyDeviceToHost);
+
+	// Print the result
+	cout << "GPU TIME : " << time << "ns\n";
+	printf("GPU result is %d \n", h_v_r[0]);
+	verify_result(h_v, n, h_v_r[0]);
+	//assert(h_v_r[0] == 65536);
+	/*for (int i = 0; i < n; i++) {
+		printf("The %d element is %d \n", i + 1, h_v[i]);
+	}*/
+
+	return 0;
+}
